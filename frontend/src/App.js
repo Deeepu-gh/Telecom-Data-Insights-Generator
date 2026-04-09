251de7ad4bec5f6837f7e66a982193469d24e785
gangaiah <140301534+Danduboina-Gangaiah@users.noreply.github.com>
2026-04-09 12:32:14 +0530

    Remove existing App component code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,1 @@
-import React from "react";
-
-function App() {
-  return (
-    <div>
-      <h1>Telecom Data Insights Generator</h1>
-      <p>Frontend UI - Under Development</p>
-    </div>
-  );
-}
-
-export default App;
+addd
