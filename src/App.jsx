3d0f028895807e89a4dbe39a02212d62b0e7ccc8
Danduboina Gangaiah <danduboina.g@incedoinc.com>
2026-04-15 12:01:11 +0530

    INitial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+   <div class="w-full h-screen  bg-orange-500 flex items-center justify-center">
+  <h1 class="text-3xl md:text-5xl lg:text-6xl font-bold text-white text-center">
+    Incedo Inc
+  </h1>
+</div>
+
+
+
+  );
+}
+
+export default App;
